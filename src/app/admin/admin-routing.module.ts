4a487b7b38c4fd75ffadf8ef5e7a3362e75b18f2
Manish <mjangir70@gmail.com>
2018-03-07 09:30:22 +0530

    moved user module to access folder

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: 'access/user',
-    loadChildren: './user/user.module#UserModule'
+    loadChildren: './access/user/user.module#UserModule'
   }
 ];
 
