ab226d803b2de5244fa739495117f81edffb9da4
Manish <mjangir70@gmail.com>
2018-03-08 02:39:01 +0530

    working on store

--- src/app/shared/store/admin/user/user.effect.ts
+++ src/app/shared/store/admin/user/user.effect.ts
@@ -3,7 +3,7 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
 import { Injectable }             from '@angular/core';
 import { Effect, Actions }        from '@ngrx/effects';
-import { Action }                 from '@ngrx/store';
+import { Action, UPDATE }         from '@ngrx/store';
 import { Observable }             from 'rxjs/Observable';
 import { of }                     from 'rxjs/observable/of';
 import {
@@ -12,7 +12,7 @@ import {
   CREATE_USER,
   DELETE_USER,
   UPDATE_USER
-}                               from './user.action';
+}                                 from './user.action';
 import {
   LoadAllUsers,
   LoadAllUsersSuccess,
@@ -29,8 +29,8 @@ import {
   DeleteUser,
   DeleteUserSuccess,
   DeleteUserError
-}                               from './user.action';
-import { User }                 from '../../../models/admin/access';
+}                                 from './user.action';
+import { User }                   from '../../../models/admin/access';
 
 
 @Injectable()
@@ -39,6 +39,9 @@ export class UserEffects {
   constructor(
     private actions$: Actions) {}
 
+  /**
+   * Load All Users
+   */
   @Effect()
   loadAllUsers$: Observable<Action> = this.actions$
     .ofType(LOAD_USERS)
@@ -46,35 +49,47 @@ export class UserEffects {
     .map(users => new LoadAllUsersSuccess(users))
     .catch((err) => of(new LoadAllUsersError(err)));
 
+  /**
+   * Load Single User
+   */
   @Effect()
   loadUser$ = this.actions$
     .ofType(LOAD_USER)
     .map((action: LoadUser) => action.payload)
     .switchMap(id => Observable.of(new User()))
     .map(user => new LoadUserSuccess(user))
-    .catch((err) => [new GetGameError(err)]);
+    .catch((err) => of(new LoadUserError(err)));
 
+  /**
+   * Create New User
+   */
   @Effect()
-  updateGame$ = this.actions$
-    .ofType(gameActions.UPDATE_GAME)
-    .map((action: UpdateGame) => action.payload)
-    .switchMap(game => this.svc.update(game))
-    .map(() => new UpdateGameSuccess())
-    .catch((err) => [new UpdateGameError(err)]);
+  createUser$ = this.actions$
+    .ofType(CREATE_USER)
+    .map((action: CreateUser) => action.payload)
+    .switchMap(newUser => Observable.of(newUser))
+    .map((response) => new CreateUserSuccess(response.id))
+    .catch((err) => of(new CreateUserError(err)));
 
+  /**
+   * Update User
+   */
   @Effect()
-  createGame$ = this.actions$
-    .ofType(gameActions.CREATE_GAME)
-    .map((action: AddGame) => action.payload)
-    .switchMap(newGame => this.svc.insert(newGame))
-    .map((response) => new AddGameSuccess(response.id))
-    .catch((err) => [new AddGameError(err)]);
+  updateUser$ = this.actions$
+    .ofType(UPDATE_USER)
+    .map((action: UpdateUser) => action.payload)
+    .switchMap(user => Observable.of(user))
+    .map(() => new UpdateUserSuccess())
+    .catch((err) => of(new UpdateUserError(err)));
 
+  /**
+   * Delete User
+   */
   @Effect()
-  removeGame$ = this.actions$
-    .ofType(gameActions.DELETE_GAME)
-    .map((action: RemoveGame) => action.payload)
-    .switchMap(id => this.svc.delete(id))
-    .map((hero: Game) => new RemoveGameSuccess(hero))
-    .catch((err) => [new RemoveGameError(err)]);
+  deleteUser$ = this.actions$
+    .ofType(DELETE_USER)
+    .map((action: DeleteUser) => action.payload)
+    .switchMap(id => Observable.of(new User()))
+    .map((user: User) => new DeleteUserSuccess(user))
+    .catch((err) => of(new DeleteUserError(err)));
 }
