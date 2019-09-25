c1c3f6a036a4a7bda32d09f5ae8d666b1bba9ff5
Vallabh Kansagara <vrkansagara@gmail.com>
2019-09-25 07:28:59 +0530

    Fully migration to angular 8

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -45,6 +45,8 @@ import 'core-js'; // <- at the top of your entry point
 /** Evergreen browsers require these. **/
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
 // import 'core-js/es7/reflect';
+import 'core-js/es/reflect';
+
 
 
 /**
